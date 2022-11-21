fb926c0105816a507a9a518747ae027d72a01411
Jacob Benson <benson.b.jacob@gmail.com>
2022-11-20 20:44:38 -0600

    Add layout

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Layout from '../layout/layout';
+
+const login = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Login</h1>
+    </Layout>
+  );
+};
+
+export default login;
---
+++ pages/register.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const register = () => {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default register;
