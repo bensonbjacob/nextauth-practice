528645e42e381216fcde4bbfa181c93ab1c47ff3
Jacob Benson <benson.b.jacob@gmail.com>
2022-11-28 13:57:43 -0600

    Add home page.

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,7 @@
+import NextAuth from 'next-auth/next';
+
+const client_id =
+  '909032561327-f24q303194806mectqma90nennrqpe8b.apps.googleusercontent.com';
+const client_secret = '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';
+
+export default NextAuth({});
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,54 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Home() {
+  const [session, setSession] = useState(false);
+
   return (
     <div>
       <Head>
         <title>Home</title>
-
-        <link rel='icon' href='/favicon.ico' />
       </Head>
-      How Bout It
+
+      {session ? User() : Guest()}
     </div>
+  );
+}
+
+//Guests
+function Guest() {
+  return (
+    <main className='container mx-auto text-center py-20'>
+      <h3 className='text-4xl font-bold'>Guest Homepage</h3>
+      <div className='flex justify-center'>
+        <p className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50'>
+          <Link href={'/login'}>Sign In</Link>
+        </p>
+      </div>
+    </main>
+  );
+}
+
+//Authorized Users
+function User() {
+  return (
+    <main className='container mx-auto text-center py-20'>
+      <h3 className='text-4xl font-bold'>User Homepage</h3>
+      <div className='details'>
+        <h5>Unknown</h5>
+        <h5>Unknown</h5>
+      </div>
+      <div className='flex justify-center'>
+        <button className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50'>
+          Sign Out
+        </button>
+      </div>
+      <div className='flex justify-center'>
+        <p className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50'>
+          <Link href={'/profile'}>Profile</Link>
+        </p>
+      </div>
+    </main>
   );
 }
