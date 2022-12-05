93d1fe0e99c35a314724d8c09939c175cfa2fad4
Jacob Benson <benson.b.jacob@gmail.com>
2022-12-05 11:02:52 -0600

    Add signup feature

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,3 +1,35 @@
+import connectMongo from '../../../database/conn';
+import Users from '../../../model/Schema';
+import { hash } from 'bcryptjs';
+
 export default async function handler(req, res) {
-  res.json({ message: 'SignUp' });
+  connectMongo().catch((error) =>
+    res.json({ error: 'Connection Failed' })
+  );
+  //Only Allow Post Method
+  if (req.method === 'POST') {
+    if (!req.body)
+      return res.status(404).json({ error: 'Missing Form Data' });
+    const { username, email, password } = req.body;
+
+    //Check For Duplicate Users
+    const checkExisting = await Users.findOne({ email });
+    if (checkExisting)
+      return res.status(422).json({ message: 'User Already Exists' });
+
+    //Hash Password
+    Users.create(
+      {
+        username,
+        email,
+        password: await hash(password, 12),
+      },
+      function (err, data) {
+        if (err) return res.status(404).json({ err });
+        res.status(201).json({ status: true, user: data });
+      }
+    );
+  } else {
+    res.status(500).json({ message: 'HTTP Method Not Valid.' });
+  }
 }
